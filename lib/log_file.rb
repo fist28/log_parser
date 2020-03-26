880db1c92e2a62cbcb647166c84c0fc1e9a97a3b
Filip Stybel <filip.stybel@justcode.io>
2020-03-26 20:38:25 +0100

    Create mocked methods for API of LogFile class
    Mocked methods:
    - views
    - unique_views

--- lib/log_file.rb
+++ lib/log_file.rb
@@ -11,6 +11,26 @@ class LogFile
     true
   end
 
+  def views
+    [
+      '/main 10 views',
+      '/page 5 views',
+    ]
+  end
+
+  def unique_views
+    [
+      '/page 5 unique views',
+      '/main 2 unique views',
+    ]
+  end
+
+  def to_s
+    views.join("\n") +
+      "\n\n" +
+      unique_views.join("\n")
+  end
+
   private
 
   def invalid_file?
--- spec/lib/log_file_spec.rb
+++ spec/lib/log_file_spec.rb
@@ -21,4 +21,45 @@ describe LogFile do
       end
     end
   end
+
+  describe 'present results of parsing' do
+    let(:file) { Pathname.new('spec/fixtures/webserver_mini.log') }
+    subject { described_class.new(file) }
+
+    before { subject.call }
+
+    describe '#page_views' do
+      it 'return array of page views' do
+        expect(subject.views).to be_a(Array)
+      end
+
+      it 'return descending sorted array' do
+        expected_result = [
+          '/main 10 views',
+          '/page 5 views'
+        ]
+        expect(subject.views).to eq(expected_result)
+      end
+    end
+
+    describe '#uniq_page_views' do
+      it 'return array of page views' do
+        expect(subject.unique_views).to be_a(Array)
+      end
+
+      it 'return descending sorted array' do
+        expected_result = [
+          '/page 5 unique views',
+          '/main 2 unique views'
+        ]
+        expect(subject.unique_views).to eq(expected_result)
+      end
+    end
+
+    describe '#to_s' do
+      it 'return block of string with results' do
+        expect(subject.to_s).to eq("/main 10 views\n/page 5 views\n\n/page 5 unique views\n/main 2 unique views")
+      end
+    end
+  end
 end
