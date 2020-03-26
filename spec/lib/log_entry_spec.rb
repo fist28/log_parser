cff16186e0a04f526217baae18c89a34f5dcd4aa
Filip Stybel <filip.stybel@justcode.io>
2020-03-26 21:18:52 +0100

    Update spec for using IP and Path class

--- spec/lib/log_entry_spec.rb
+++ spec/lib/log_entry_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 require 'rspec'
 
 describe LogEntry do
-  subject { described_class.new(path: '/foo', ip: '0.0.0.0') }
+  let(:path) { Path.new('/foo') }
+  let(:ip) { IP.new('0.0.0.0') }
+  subject { described_class.new(path: path, ip: ip) }
 
   it 'has path attribute' do
-    expect(subject.path).to eq('/foo')
+    expect(subject.path).to eq(path)
   end
 
   it 'has ip attribute' do
-    expect(subject.ip).to eq('0.0.0.0')
+    expect(subject.ip).to eq(ip)
   end
 end
