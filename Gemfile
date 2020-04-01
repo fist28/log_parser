532114287efb6679e75db08693dd8de54708c56a
Filip Stybel <filip.stybel@justcode.io>
2020-04-01 17:19:59 +0200

    Add SimpleCov for checking code coverage in project

--- Gemfile
+++ Gemfile
@@ -1,1 +1,2 @@
 gem 'rspec', '~> 3.4', group: [:development, :test]
+gem 'simplecov', require: false, group: :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
+require 'simplecov'
+require 'byebug'
+SimpleCov.start
 project_root = File.dirname(File.absolute_path(__FILE__))
 Dir.glob(project_root + '/../lib/*.rb') { |file| require file }
